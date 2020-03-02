73869ec5b070468a7d9e276166c3455197751122
Christoforos Varakliotis <c.varakliotis@gmail.com>
2020-03-02 19:31:06 +0200

    setup eslint

---
+++ .eslintrc.js
@@ -0,0 +1,16 @@
+module.exports = {
+	root: true,
+	parser: '@typescript-eslint/parser',
+	plugins: ['@typescript-eslint', 'react'],
+	extends: [
+		'eslint:recommended',
+		'plugin:@typescript-eslint/eslint-recommended',
+		'plugin:@typescript-eslint/recommended',
+		'plugin:react/recommended',
+	],
+	settings: {
+		react: {
+			version: 'detect',
+		},
+	},
+};
