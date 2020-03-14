5257d4d6ba15487f4e3b4a17506dfd2cd9ab577a
Christoforos Varakliotis <c.varakliotis@gmail.com>
2020-03-14 20:42:36 +0200

    publish preps  🚚

---
+++ src/index.ts
@@ -0,0 +1,7 @@
+export { default as Context } from './context';
+export { default as Provider } from './Provider';
+export { default as useRxCollection } from './useRxCollection';
+export { default as useRxData } from './useRxData';
+export { default as useRxDB } from './useRxDB';
+export { default as useRxDocument } from './useRxDocument';
+export { default as useRxQuery } from './useRxQuery';
