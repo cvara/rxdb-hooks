fc188597b72aeedd6fba32dbb5d0385e7a5e0933
Christoforos Varakliotis <c.varakliotis@gmail.com>
2020-03-14 11:49:46 +0200

    new hook: useRxQuery

--- src/useRxData.ts
+++ src/useRxData.ts
@@ -1,180 +1,56 @@
-/* eslint @typescript-eslint/camelcase: 0 */
-import { useEffect, useCallback, useReducer, Reducer } from 'react';
-import { RxCollection, RxQuery, isRxQuery, RxDocument } from 'rxdb';
+import { useMemo } from 'react';
+import { RxCollection, RxQuery } from 'rxdb';
 import useRxCollection from './useRxCollection';
-
-interface RxState<T> {
-	result: T[] | RxDocument<T>[];
-	isFetching: boolean;
-	exhausted: boolean;
-	limit: number;
-}
-
-interface RxData<T> extends RxState<T> {
-	fetchMore: () => void;
-	resetList: () => void;
-}
-
-interface RxDataJSON<T> extends RxData<T> {
-	result: T[];
-}
-
-interface RxDataDoc<T> extends RxData<T> {
-	result: RxDocument<T>[];
-}
-
-interface UseRxDataOptions {
-	pageSize?: number;
-	sortBy?: string;
-	sortOrder?: 'asc' | 'desc';
-	json?: boolean;
-}
+import useRxQuery, {
+	UseRxQueryOptions,
+	RxQueryResult,
+	RxQueryResultJSON,
+	RxQueryResultDoc,
+} from './useRxQuery';
 
 type QueryConstructor<T> = (
 	collection: RxCollection<T>
 ) => RxQuery<T> | undefined;
 
-enum ActionType {
-	Reset = 'Reset',
-	FetchMore = 'FetchMore',
-	FetchSuccess = 'FetchSuccess',
-}
-
-interface ResetAction {
-	type: ActionType.Reset;
-	pageSize: number;
-}
-
-interface FetchMoreAction {
-	type: ActionType.FetchMore;
-	pageSize: number;
-}
-
-interface FetchSuccessAction<T> {
-	type: ActionType.FetchSuccess;
-	docs: T[];
-}
-
-type AnyAction<T> = ResetAction | FetchMoreAction | FetchSuccessAction<T>;
-
-const reducer = <T>(state: RxState<T>, action: AnyAction<T>): RxState<T> => {
-	switch (action.type) {
-		case ActionType.Reset:
-			return {
-				...state,
-				result: [],
-				isFetching: true,
-				limit: action.pageSize,
-			};
-		case ActionType.FetchMore:
-			return {
-				...state,
-				isFetching: true,
-				limit: state.limit + action.pageSize,
-			};
-		case ActionType.FetchSuccess:
-			return {
-				...state,
-				result: action.docs,
-				isFetching: false,
-				exhausted: !state.limit || action.docs.length < state.limit,
-			};
-	}
-};
+function useRxData<T>(
+	collectionName: string,
+	queryConstructor: QueryConstructor<T> | undefined
+): RxQueryResultDoc<T>;
 
 function useRxData<T>(
 	collectionName: string,
-	queryConstructor?: QueryConstructor<T>,
-	options?: UseRxDataOptions & { json: true }
-): RxDataJSON<T>;
+	queryConstructor: QueryConstructor<T> | undefined,
+	options?: UseRxQueryOptions & { json?: false }
+): RxQueryResultDoc<T>;
 
 function useRxData<T>(
 	collectionName: string,
-	queryConstructor?: QueryConstructor<T>,
-	options?: UseRxDataOptions & { json?: false }
-): RxDataDoc<T>;
+	queryConstructor: QueryConstructor<T> | undefined,
+	options?: UseRxQueryOptions & { json: true }
+): RxQueryResultJSON<T>;
 
 /**
- * Subscribes to specified query and provides results, also providing:
- *  - state indicators for fetching and list depletion
- *  - a fetchMore callback function for pagination support
- *  - a resetList callback function for conveniently reseting list data
+ * Convenience wrapper around useRxQuery that expects a collection name
+ * & a query constructor function
  */
 function useRxData<T>(
 	collectionName: string,
-	queryConstructor?: QueryConstructor<T>,
-	options: UseRxDataOptions = {}
-): RxData<T> {
-	const { pageSize = 0, sortBy, sortOrder = 'desc', json } = options;
-
+	queryConstructor: QueryConstructor<T> | undefined,
+	options: UseRxQueryOptions = {}
+): RxQueryResult<T> {
 	const collection = useRxCollection<T>(collectionName);
 
-	const initialState = {
-		result: [],
-		limit: pageSize,
-		isFetching: true,
-		exhausted: false,
-	};
-
-	const [state, dispatch] = useReducer<Reducer<RxState<T>, AnyAction<T>>>(
-		reducer,
-		initialState
-	);
-
-	const fetchMore = useCallback(() => {
-		if (!state.limit || state.isFetching || state.exhausted) {
-			return;
-		}
-		dispatch({ type: ActionType.FetchMore, pageSize });
-	}, [state.limit, state.isFetching, state.exhausted]);
-
-	const resetList = useCallback(() => {
-		if (!state.limit || state.limit <= pageSize) {
-			return;
-		}
-		dispatch({ type: ActionType.Reset, pageSize });
-	}, [pageSize, state.limit]);
-
-	useEffect(() => {
+	const query = useMemo(() => {
 		if (!collection || typeof queryConstructor !== 'function') {
-			return;
+			return null;
 		}
+		return queryConstructor(collection);
+	}, [collection, queryConstructor]);
 
-		let query = queryConstructor(collection);
-		if (!isRxQuery(query)) {
-			return;
-		}
-
-		if (state.limit) {
-			query = query.limit(state.limit);
-		}
-
-		if (sortBy) {
-			query = query.sort({
-				[sortBy]: sortOrder,
-			});
-		}
-
-		const sub = query.$.subscribe(
-			(documents: RxDocument<T>[] | RxDocument<T>) => {
-				const docs = Array.isArray(documents) ? documents : [documents];
-				dispatch({
-					type: ActionType.FetchSuccess,
-					docs: json ? docs.map(doc => doc.toJSON()) : docs,
-				});
-			}
-		);
-
-		return (): void => {
-			sub.unsubscribe();
-		};
-	}, [queryConstructor, collection, state.limit, sortBy, sortOrder]);
-
-	return {
-		...state,
-		fetchMore,
-		resetList,
-	};
+	// get around type-narrowing issue
+	return options.json
+		? useRxQuery(query, { ...options, json: true })
+		: useRxQuery(query, { ...options, json: false });
 }
 
 export default useRxData;
---
+++ src/useRxQuery.ts
@@ -0,0 +1,171 @@
+import { useEffect, useCallback, useReducer, Reducer } from 'react';
+import { RxQuery, RxDocument, isRxQuery } from 'rxdb';
+
+interface RxState<T> {
+	result: T[] | RxDocument<T>[];
+	isFetching: boolean;
+	exhausted: boolean;
+	limit: number;
+}
+
+export interface RxQueryResult<T> extends RxState<T> {
+	fetchMore: () => void;
+	resetList: () => void;
+}
+
+export interface RxQueryResultJSON<T> extends RxQueryResult<T> {
+	result: T[];
+}
+
+export interface RxQueryResultDoc<T> extends RxQueryResult<T> {
+	result: RxDocument<T>[];
+}
+
+export interface UseRxQueryOptions {
+	pageSize?: number;
+	sortBy?: string;
+	sortOrder?: 'asc' | 'desc';
+	json?: boolean;
+}
+
+enum ActionType {
+	Reset = 'Reset',
+	FetchMore = 'FetchMore',
+	FetchSuccess = 'FetchSuccess',
+}
+
+interface ResetAction {
+	type: ActionType.Reset;
+	pageSize: number;
+}
+
+interface FetchMoreAction {
+	type: ActionType.FetchMore;
+	pageSize: number;
+}
+
+interface FetchSuccessAction<T> {
+	type: ActionType.FetchSuccess;
+	docs: T[];
+}
+
+type AnyAction<T> = ResetAction | FetchMoreAction | FetchSuccessAction<T>;
+
+const reducer = <T>(state: RxState<T>, action: AnyAction<T>): RxState<T> => {
+	switch (action.type) {
+		case ActionType.Reset:
+			return {
+				...state,
+				result: [],
+				isFetching: true,
+				limit: action.pageSize,
+			};
+		case ActionType.FetchMore:
+			return {
+				...state,
+				isFetching: true,
+				limit: state.limit + action.pageSize,
+			};
+		case ActionType.FetchSuccess:
+			return {
+				...state,
+				result: action.docs,
+				isFetching: false,
+				exhausted: !state.limit || action.docs.length < state.limit,
+			};
+	}
+};
+
+function useRxQuery<T>(query: RxQuery): RxQueryResultDoc<T>;
+
+function useRxQuery<T>(
+	query: RxQuery,
+	options?: UseRxQueryOptions & { json?: false }
+): RxQueryResultDoc<T>;
+
+function useRxQuery<T>(
+	query: RxQuery,
+	options?: UseRxQueryOptions & { json: true }
+): RxQueryResultJSON<T>;
+
+/**
+ * Subscribes to specified query and provides results, also providing:
+ *  - state indicators for fetching and list depletion
+ *  - a fetchMore callback function for pagination support
+ *  - a resetList callback function for conveniently reseting list data
+ */
+function useRxQuery<T>(
+	query: RxQuery,
+	options: UseRxQueryOptions = {}
+): RxQueryResult<T> {
+	const { pageSize = 0, sortBy, sortOrder = 'desc', json } = options;
+
+	const initialState = {
+		result: [],
+		limit: pageSize,
+		isFetching: true,
+		exhausted: false,
+	};
+
+	const [state, dispatch] = useReducer<Reducer<RxState<T>, AnyAction<T>>>(
+		reducer,
+		initialState
+	);
+
+	const { result, limit, isFetching, exhausted } = state;
+
+	const fetchMore = useCallback(() => {
+		if (!limit || isFetching || exhausted) {
+			return;
+		}
+		dispatch({ type: ActionType.FetchMore, pageSize });
+	}, [limit, isFetching, exhausted, pageSize]);
+
+	const resetList = useCallback(() => {
+		if (!limit || limit <= pageSize) {
+			return;
+		}
+		dispatch({ type: ActionType.Reset, pageSize });
+	}, [pageSize, limit]);
+
+	useEffect(() => {
+		if (!isRxQuery(query)) {
+			return;
+		}
+
+		if (limit) {
+			query = query.limit(limit);
+		}
+
+		if (sortBy) {
+			query = query.sort({
+				[sortBy]: sortOrder,
+			});
+		}
+
+		const sub = query.$.subscribe(
+			(documents: RxDocument<T>[] | RxDocument<T>) => {
+				const docs = Array.isArray(documents) ? documents : [documents];
+				dispatch({
+					type: ActionType.FetchSuccess,
+					docs: json ? docs.map(doc => doc.toJSON()) : docs,
+				});
+			}
+		);
+
+		return (): void => {
+			sub.unsubscribe();
+		};
+	}, [query, limit, sortBy, sortOrder]);
+
+	return {
+		result,
+		limit,
+		isFetching,
+		exhausted,
+		fetchMore,
+		resetList,
+	};
+}
+
+export default useRxQuery;
