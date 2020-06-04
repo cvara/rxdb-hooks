b732907e0eabad754f96e138f7c9319287aaa393
Christoforos Varakliotis <c.varakliotis@gmail.com>
2020-06-04 11:30:04 +0300

    changelog

---
+++ bin/changelog.js
@@ -0,0 +1,8 @@
+require('dotenv').config();
+const exec = require('shelljs').exec;
+
+exec('docker pull ferrarimarco/github-changelog-generator', { silent: false });
+exec(
+	`docker run --rm -v "$(pwd)":/usr/local/src/your-app ferrarimarco/github-changelog-generator --user=cvara --project=rxdb-hooks --token=${process.env.GH_ACCESS_TOKEN} --since-tag=1.0.0`,
+	{ silent: false }
+);
